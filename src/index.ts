558c771b05152768e2d38419b4b4c2bbe86285c0
Guillaume Regimbeau <gregimbeau@mail.com>
2024-01-04 15:16:12 +0100

    api ok

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import appointmentRoutes from "./routes/appointmentRoutes";
+import cors from "cors";
+
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", appointmentRoutes);
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
