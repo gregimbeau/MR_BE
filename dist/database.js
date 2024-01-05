38cb19ee5c5f00831f9f74497802bbcd46302e71
Guillaume Regimbeau <gregimbeau@mail.com>
2024-01-05 13:16:31 +0100

    prod var

--- dist/database.js
+++ dist/database.js
@@ -1,11 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
+const isProduction = process.env.NODE_ENV === "production";
 const pool = new pg_1.Pool({
-    user: "username",
-    host: "localhost",
-    database: "modaresa",
-    password: "password",
-    port: 5432,
+    user: isProduction ? process.env.DB_USER : "username",
+    host: isProduction ? process.env.DB_HOST : "localhost",
+    database: isProduction ? process.env.DB_NAME : "modaresa",
+    password: isProduction ? process.env.DB_PASSWORD : "password",
+    port: isProduction ? parseInt(process.env.DB_PORT, 10) : 5432,
 });
 exports.default = pool;
--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,13 @@
 import { Pool } from "pg";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
-  user: "username",
-  host: "localhost",
-  database: "modaresa",
-  password: "password",
-  port: 5432,
+  user: isProduction ? process.env.DB_USER : "username",
+  host: isProduction ? process.env.DB_HOST : "localhost",
+  database: isProduction ? process.env.DB_NAME : "modaresa",
+  password: isProduction ? process.env.DB_PASSWORD : "password",
+  port: isProduction ? parseInt(process.env.DB_PORT, 10) : 5432,
 });
 
 export default pool;
